276ce9718ffa9f601006400265aa85c6ddb2dabe
adamascencio <ascencioadam@gmail.com>
2024-08-16 15:19:33 -0700

    feat: add endpoint that responds with unix time string and utc date string when the server receives a GET /api/:date request with a valid date

--- index.js
+++ index.js
@@ -14,17 +14,24 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", (req, res) => {
+  const date = req.params.date;
+  const newDate = new Date(date);
+
+  if (newDate == "Invalid Date") {
+    return res.json({error: "Invalid Date"});
+  }
+
+  res.json({
+    unix: newDate.getTime(),
+    utc: newDate.toUTCString(),
+  });
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 const listener = app.listen(process.env.PORT || 3000, () => {
